fdc896a5d711e61d102b852f9ff10350ca143663
Juan Andrés Galicia <69065800+JuanGalicia20@users.noreply.github.com>
2021-02-15 22:53:19 -0600

    Ordenamientos terminados
    Se terminaron de implementar los 5 ordenamientos

--- Ordenamientos.java
+++ Ordenamientos.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Ordenamientos {
-
+public class Ordenamientos {	
+	
 	//-----------------------Bubble Sort----------------------------------------
 	public Comparable[] bubbleSort(Comparable[] lista)
 	{
@@ -82,51 +83,129 @@ public class Ordenamientos {
         }
         return inputArray;
     }
-	
-	/**
-	 * Metodo para ordenar arreglos utilizando algoritmo Gnome
-	 * @param array arreglo desordenado
-	 */
-	public void gnome_sort(Integer[] array) {
-		
-		//Ciclo que se mantiene mientras i sea menor que el numero de elementos del arreglo
-		for(int i = 1; i < array.length;) {
-			
-			//Condicion si el valor en indice i-1 es menor o igual al valor en indice i
-			if(array[i-1].compareTo(array[i]) != 1){
-				
-				//Pasa al siguiente valor en la lista ya que esta pareja ya está ordenada
+    
+    
+    //------------------Gnome Sort------------------------------
+    public Comparable[] gnomeSort(Comparable[] arr) 
+    { 
+		int i = 1;
+		while(i<arr.length)
+		{
+			if(arr[i-1].compareTo(arr[i])<=0)
+			{
 				i++;
 			}
-			
-			//Condicion si el valor del indice i-1 es mayor al valor en indice i
-			else {
-				
-				//Se crea variable temporal tipo Integer
-				Integer temp;
-				
-				//La variable temporal toma el valor del indice i -1
-				temp = array[i-1];
-				
-				//El valor en indice i-1 toma el valor en indice i
-				array[i-1] = array[i];
-				
-				//El valor en indice i toma el valor temporal
-				array[i] = temp;
-				
-				//Se reduce i en 1 para revisar la pareja anterior ya que estos valores intercambiaron
-				i--;
+			else
+			{
+				Comparable n = arr[i-1];
+				arr[i-1]=arr[i];
+				arr[i]=n;
+				if(--i==0)
+				{
+					i=1;
+				}
 			}
-			
-			//Condición en caso i llegue a ser 0
-			if(i == 0) {
-				
-				//Ya que se evalua el indice i-1, esto daría error, entonces nuevamente i = 1
-				i = 1;
-			}
-			
 		}
-	}
+		return arr;
+    } 
+    
+    
+    
+    
+    //-----------------QuickSort-----------------------------------
+    public int partition(Comparable[] arr,int low,int high) 
+    {
+        Comparable pivot = arr[high];  
+        int i = (low-1); // index of smaller element 
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than the pivot 
+            if (arr[j].compareTo(pivot)==-1) 
+            { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                Comparable temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+        Comparable temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+  
+  
+    /* The main function that implements QuickSort() 
+      arr[] --> Array to be sorted, 
+      low  --> Starting index, 
+      high  --> Ending index */
+    public Comparable[] sort(Comparable[] arr,int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            /* pi is partitioning index, arr[pi] is  
+              now at right place */
+            int pi = partition(arr, low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            sort(arr, low, pi-1); 
+            sort(arr, pi+1, high); 
+        } 
+        return arr;
+    } 
 	
-	
+    
+    //-------------------Radix Sort------------------------
+    public Comparable max(Comparable[] arr, int n){
+        Comparable mayor = arr[0];
+        for (int i=1; i<n; i++){
+            if (arr[i].compareTo(mayor)==1){
+                mayor = arr[i];
+            }
+        }
+        return mayor;
+    }
+    public Comparable[] count(Comparable[] arr, int n, int exp){
+        int[] fArray = new int[n]; 
+        int i;
+        int[] cArray = new int[10];
+        Arrays.fill(cArray, 0);
+        
+
+        for (i=0; i<n; i++){
+        	cArray[(((int)arr[i])/exp)%10]++; //guarda el conteo
+        }
+        // Cambiar la cuenta para que contenga la posicion actual del digito de salida
+        for (i=1; i<10; i++){
+        	cArray[i]+= cArray[i-1];
+        }
+        //Construir el array ordenado
+        for (i=n-1; i>=0; i--){
+        	fArray[cArray[((int)arr[i]/exp)%10]-1] = (int)arr[i];
+        	cArray[((int)arr[i]/exp)%10]--;
+        }
+        //Copiar el array ordenado para que el original contenga el ordenado
+        for (i=0; i<n; i++){
+        	arr[i] = fArray[i];
+        }
+        return arr;
+    }
+    public Comparable[] radix(Comparable[] arr){
+        int n = arr.length;
+        //Encuentra el numero mayor 
+        Comparable mayorm = max(arr, n);
+        int mayormo= (int)mayorm;
+        // Contar ordenando cada digito 
+        for (int exp=1; mayormo/exp>0; exp*=10){
+            count(arr, n, exp);
+        }
+        return arr;
+    }
+    
 }
